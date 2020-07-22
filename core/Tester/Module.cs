bf51161b9c4d5f5bbe6e134d16a5ce0da974325d
jysummers <54165586+jysummers@users.noreply.github.com>
2020-07-22 14:11:53 +0800

    initial commit.

---
+++ core/Tester/Module.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace Tester
+{
+    public class Module
+    {
+        public void Run()
+        {
+            throw new InvalidOperationException("This is only a test.");
+        }
+    }
+}
